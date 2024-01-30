4d28c99d28e1f190c8eca9a7d2f99cc52f4f8565
Mikhail Delyanchenko <mikhail.delyanchenko@gmail.com>
2024-01-30 18:34:53 +0300

    Created Title, Genre, Category models. Created reviews app and added to settings. Included api urls in main app urls

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,7 +21,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api.apps.ApiConfig'
+    'rest_framework',
+    'api.apps.ApiConfig',
+    'reviews.apps.ReviewsConfig',
 ]
 
 MIDDLEWARE = [
@@ -102,3 +104,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
+
+REST_FRAMEWORK = {
+}
---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,84 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+MAX_CHARS_LENGTH = 256
+MAX_SLUG_LENGTH = 50
+
+
+class BaseModelsMixin(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=MAX_CHARS_LENGTH,
+        blank=False,
+    )
+    slug = models.SlugField(
+        verbose_name='Слаг',
+        max_length=MAX_SLUG_LENGTH,
+        unique=True,
+        blank=False,
+    )
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class Category(BaseModelsMixin):
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+
+
+class Genre(BaseModelsMixin):
+
+    class Meta:
+        verbose_name = 'жанр'
+        verbose_name_plural = 'Жанры'
+
+
+class Title(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=MAX_CHARS_LENGTH,
+        blank=False,
+    )
+    year = models.IntegerField(
+        verbose_name='Год выпуска',
+        blank=False,
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+        blank=True,
+    )
+    # genre = models.ForeignKey(
+    #     'Genre',
+    #     models.SET_NULL,
+    #     blank=False,
+    #     null=True,
+    # )
+    genre = models.ManyToManyField(
+        'Genre',
+        verbose_name='Жанры',
+        related_name='titles',
+        blank=False,
+    )
+    category = models.ForeignKey(
+        'Category',
+        models.SET_NULL,
+        related_name='posts',
+        blank=False,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = 'произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name
